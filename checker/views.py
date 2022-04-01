5e0be3ffa49ffa60b0b01794c14c24a4df4f8327
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2022-04-01 09:57:01 +0100

    Add basic paginated view.

--- checker/views.py
+++ checker/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.db.models import Max
+from django.core.paginator import Paginator
 from .models import PipelineApp, PipelineEnv
 
 def home(request):
     last_scan_time = PipelineApp.objects.aggregate(Max('scan_start_time'))["scan_start_time__max"]
     pipeline_envs = PipelineEnv.objects.filter(config_id_fk__scan_start_time=last_scan_time)
+    paginator = Paginator(pipeline_envs, 10)
+    page = request.GET.get('page', 1)
     return render(request, 'home.html', {
         'last_scan_time' : last_scan_time,
-        'pipeline_envs' : pipeline_envs,
+        'pipeline_envs' : paginator.page(page),
         }
     )
