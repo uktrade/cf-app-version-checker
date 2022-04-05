70fa12f1d611083f99151579b051b0c119a3c858
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2022-04-05 14:34:26 +0100

    Increase default view page size.

--- checker/views.py
+++ checker/views.py
@@ -6,7 +6,7 @@ from .models import PipelineApp, PipelineEnv
 def home(request):
     last_scan_time = PipelineApp.objects.aggregate(Max('scan_start_time'))["scan_start_time__max"]
     pipeline_envs = PipelineEnv.objects.filter(config_id_fk__scan_start_time=last_scan_time)
-    paginator = Paginator(pipeline_envs, 10)
+    paginator = Paginator(pipeline_envs, 100)
     page = request.GET.get('page', 1)
     return render(request, 'home.html', {
         'last_scan_time' : last_scan_time,
